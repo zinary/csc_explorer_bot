9b433b643002566e040c1135803074ba5ad2a6a2
Ram Kumar <ram7953@gmail.com>
2022-02-20 12:27:29 +0530

    FIX: spacing

--- bot.py
+++ bot.py
@@ -247,7 +247,6 @@ def balances(update: Update, context: CallbackContext) -> None:
 
             for crc20 in balance_list.crc20:
                 message += f"""
-
 <b>Name :</b> {crc20.token_info.name}
 <b>Symbol :</b> {crc20.token_info.symbol}
 <b>Address :</b> <code> \n{crc20.token_info.contract} </code>
